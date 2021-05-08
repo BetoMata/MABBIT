d8b6a561c9caa25f148dc1a10926d089c7dfa74e
BetoMata29 <rmata29@alumnos.uaq.mx>
2021-05-08 11:23:33 -0500

    Creación del Proyecto Base de Flutter

--- README.md
+++
@@ -1,2 +0,0 @@
-# MABBIT
-Proyecto de desarrollo Móvil sobre aplicación de creación de hábitos   
---
+++ mabbit/android/app/src/main/kotlin/com/example/mabbit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mabbit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
