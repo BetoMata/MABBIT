3688d2b343d0d7c5ffbfa21b355a6df03306ad48
BetoMata29 <rmata29@alumnos.uaq.mx>
2021-05-25 20:44:36 -0500

    Carga de archivos
    Creación de la aplicación completa

---
+++ mabbit_app/android/app/src/main/kotlin/com/example/mabbit_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mabbit_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mabbit_app/lib/model/upper_body_workout.dart
@@ -0,0 +1,76 @@
+class UpperBodyWorkout {
+  final String imagePath, name, instruction;
+
+  UpperBodyWorkout({this.imagePath, this.name, this.instruction});
+}
+
+final upperBody = [
+  [
+    UpperBodyWorkout(
+      imagePath: "assets/chest.png",
+      name: "Barras",
+      instruction: "3 sets - 6 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "assets/chest.png",
+      name: "Pesas",
+      instruction: "4 sets - 6 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "assets/chest.png",
+      name: "Dips",
+      instruction: "2 sets - 10 repitions",
+    ),
+  ],
+  [
+    UpperBodyWorkout(
+      imagePath: "assets/back.png",
+      name: "Dominadas",
+      instruction: "2 sets - 8 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "assets/back.png",
+      name: "Peso muerto",
+      instruction: "2 sets - 4 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "assets/back.png",
+      name: "Lat pulldown",
+      instruction: "4 sets - 6 repitions",
+    ),
+  ],
+  [
+    UpperBodyWorkout(
+      imagePath: "assets/chest.png",
+      name: "Barras",
+      instruction: "3 sets - 6 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "assets/chest.png",
+      name: "pesto_pasta",
+      instruction: "4 sets - 6 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "assets/chest.png",
+      name: "Fondo",
+      instruction: "2 sets - 10 repitions",
+    ),
+  ],
+  [
+    UpperBodyWorkout(
+      imagePath: "assets/back.png",
+      name: "Dominadas",
+      instruction: "2 sets - 8 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "assets/back.png",
+      name: "Peso muerto",
+      instruction: "2 sets - 4 repitions",
+    ),
+    UpperBodyWorkout(
+      imagePath: "assets/back.png",
+      name: "Polea",
+      instruction: "4 sets - 6 repitions",
+    ),
+  ],
+];
